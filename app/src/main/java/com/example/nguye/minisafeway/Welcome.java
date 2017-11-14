050b70410747699cd83ff6658873f2d866406540
MrJc010 <32074443+MrJc010@users.noreply.github.com>
2017-11-13 21:30:38 -0800

    I did nothing :))))))))))

--- app/src/main/java/com/example/nguye/minisafeway/Welcome.java
+++ app/src/main/java/com/example/nguye/minisafeway/Welcome.java
@@ -6,23 +6,27 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Welcome extends AppCompatActivity {
 
     private ImageView iv;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         iv = (ImageView) findViewById(R.id.image);
+        text = (TextView) findViewById(R.id.text);
         Animation wel = AnimationUtils.loadAnimation(this,R.anim.mytransition);
         iv.startAnimation(wel);
+        text.startAnimation(wel);
         final Intent i = new Intent(this,Signin.class);
         Thread timer = new Thread(){
             public void run(){
                 try {
-                    sleep(5000);
+                    sleep(6000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
