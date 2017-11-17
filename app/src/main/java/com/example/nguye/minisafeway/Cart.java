260b8b47fe20285a08bf11c38d9ba6812729484a
vincephng <vincephng@gmail.com>
2017-11-17 14:11:48 -0800

    Merge branch 'phuc' of /Users/vincephng/AndroidStudioProjects/CS133_MiniSafeWay with conflicts.

--- app/src/main/java/com/example/nguye/minisafeway/Cart.java
+++ app/src/main/java/com/example/nguye/minisafeway/Cart.java
@@ -1,13 +1,16 @@
 package com.example.nguye.minisafeway;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -32,11 +35,14 @@ public class Cart extends AppCompatActivity {
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
 
-    FirebaseDatabase database;
-    DatabaseReference requests;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    String currentUser = Common.currentUser.getId().toString();
+
+    DatabaseReference requests = database.getReference("History");
 
     TextView txtTotalPrice;
-    Button btnPlace;
+    Button btnPlace,home;
 
     List<Order> cart = new ArrayList<>();
 
@@ -46,9 +52,12 @@ public class Cart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+        System.out.println(currentUser); //TESTING TO SEE IF THIS IS NULL
 
-        database = FirebaseDatabase.getInstance();
-        requests = database.getReference("Requests");
+        //requests = database.getReference("User");
+
+
+
 
         recyclerView = (RecyclerView) findViewById(R.id.listCart);
         recyclerView.setHasFixedSize(true);
@@ -61,7 +70,11 @@ public class Cart extends AppCompatActivity {
         btnPlace.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showAlertDialog();
+                if(cart.size() > 0) {
+                    showAlertDialog();
+                }else{
+                    Toast.makeText(Cart.this,"Your cart is empty!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         
@@ -80,20 +93,27 @@ public class Cart extends AppCompatActivity {
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT
         );
+        final EditText bank = new EditText(Cart.this);
+        LinearLayout.LayoutParams lp1 = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
         edtAddress.setLayoutParams(lp);
+        bank.setLayoutParams(lp1);
         alertDialog.setView(edtAddress);
+        alertDialog.setView(bank);
         alertDialog.setIcon(R.drawable.ic_shopping_cart_black_24dp);
 
         alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 Request request = new Request(
-                        Common.currentUser.getName(),
-                        edtAddress.getText().toString(),
-                        txtTotalPrice.getText().toString(),
                         cart
                 );
-                requests.child(String.valueOf(System.currentTimeMillis())).setValue(request);
+                for(int  x = 0 ; x < cart.size(); x++){
+                    requests.push().setValue(cart.get(x));
+                }
+
 
                 //Delete cart
                 new Database(getBaseContext()).cleanCart();
@@ -111,18 +131,50 @@ public class Cart extends AppCompatActivity {
         });
         alertDialog.show();
     }
+    private void onCheckboxClicked(MenuItem item) {
+
+        boolean checked = ((CheckBox) ).isChecked();
+        switch(item.getOrder()) {
+            case R.id.checkbox:
+                if (checked) {
+                deleteCart(item.getOrder());
+
+                } else {
+
+                }
+                break;
+        }
 
     private void loadListFood() {
         cart = new Database(this).getCarts();
         adapter = new CartAdapter(cart, this);
+        adapter.notifyDataSetChanged();
         recyclerView.setAdapter(adapter);
-        int total = 0;
+        double total = 0;
         for(Order order:cart){
-            total += (Integer.parseInt(order.getPrice()))*(Integer.parseInt(order.getQuantity()))-(Integer.parseInt(order.getDiscount()));
+            total += (Double.parseDouble(order.getPrice()))*(Double.parseDouble(order.getQuantity()));
         }
         Locale locale = new Locale("en","US");
         NumberFormat fmt = NumberFormat.getCurrencyInstance(locale);
 
         txtTotalPrice.setText(fmt.format(total));
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if(item.getTitle().equals(Common.DELETE)){
+            deleteCart(item.getOrder());
+        }
+        return true;
+    }
+
+    private void deleteCart(int position) {
+        cart.remove(position);
+        new Database(this).cleanCart();
+        for(Order item : cart){
+            new Database(this).addToCart(item);
+        }
+        loadListFood();
     }
 }
